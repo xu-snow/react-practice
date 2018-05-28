c1122d948d8b7ffa4cdc62d692a5e4272f920995
zhengxu <332171564@qq.com>
2018-05-29 01:52:41 +0800

    async-wrap;commonsChunkPlugin

---
+++ src/async-component.tsx
@@ -0,0 +1,44 @@
+/*
+ * @Author: zhengxu 
+ * @Date: 2018-05-29 15:08:10 
+ * @Last Modified by: zhengxu
+ * @Last Modified time: 2018-05-29 21:53:16
+ */
+
+/**
+ * for react router4 code splitting
+ */
+
+import * as React from 'react'
+import { RouteComponentProps } from 'react-router-dom'
+
+type AsyncImportComponent = () => Promise<any>
+
+export default (loadComponent: AsyncImportComponent, placeholder = null): React.ComponentClass<RouteComponentProps<any>> => {
+  return class AsyncComponent extends React.Component<RouteComponentProps<any>, { component: any }>{
+    private unmount: boolean = false
+    constructor(props: RouteComponentProps<any>) {
+      super(props)
+      this.state = {
+        component: null
+      }
+    }
+
+    public componentWillUnmount() {
+      this.unmount = false
+    }
+
+    public async  componentDidMount() {
+      const { default: component } = await loadComponent()
+      if (this.unmount) { return }
+      this.setState({
+        component
+      })
+    }
+
+    public render() {
+      const C = this.state.component
+      return C ? <C {...this.props} /> : placeholder
+    }
+  }
+}
